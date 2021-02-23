845c4f7902c2c58a536fddd1b5b456117ee8c628
rogerterrazas <rogert9801@gmail.com>
2021-02-22 22:31:52 -0600

    implementing boiler plate resume page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-netlify-cms`],
+  siteMetadata: {
+    title: `Gatsby Starter`,
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`, 
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src`,
+    },
+  },
+],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return ( 
+  <Layout>
+    <h1>Welcome to  {data.site.siteMetadata.title}</h1>
+  </Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
